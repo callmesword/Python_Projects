08d71cfdd7800324fbb835b85591232dc7a841e2
0XLAYA <houssamlahyani0@gmail.com>
2020-09-17 13:11:38 +0100

    num guessing game

---
+++ num_guessing.py
@@ -0,0 +1,34 @@
+import random
+# a function to compute the difference between the guessed num and the random num
+def diff(usr_inp,random_num):
+	return random_num - usr_inp
+# stores the random num
+random_num = random.randint(1,100)
+# the user is guessing
+usr_guess = int(input('Guess a number between 1 and 100: '))
+# the allowed attempts 
+attempts = 3
+
+diff = diff(random_num,usr_guess)
+# This loop checks if the gussed num == the random
+# the loop will break if the user failed to guess the number after 3 attempts
+while attempts > 1:
+	
+	if usr_guess == random_num:
+		print("Good job! You Guessed it!")
+	elif diff < 10:
+		attempts -= 1
+		print('So close! Try {} more time(s)'.format(attempts))
+		usr_guess = int(input('Guess a number between 1 and 100: '))
+	
+else:
+	print("GAME OVER!")
+
+
+
+		
+
+	
+
+
+
