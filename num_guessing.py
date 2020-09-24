99a8e0af004aa4675d58432f762966d5d258823c
0XLaya <61026156+0XLAYA@users.noreply.github.com>
2020-09-24 13:58:25 +0100

    Update num_guessing.py

--- num_guessing.py
+++ num_guessing.py
@@ -1,15 +1,21 @@
 import random
-# a function to compute the difference between the guessed num and the random num
-def diff(usr_inp,random_num):
-    return random_num - usr_inp
-# stores the random num
-random_num = random.randint(1,2)
-# the user is guessing
-usr_guess = int(input('Guess a number between 1 and 100: '))
-# the allowed attempts 
-attempts = 3
 
-diff = diff(random_num,usr_guess)
+#Welcome message
+print('Welcome! Please enter the range of nums you want and number of attempts')
+
+#the range the user wants/ 'F' means from, 'T' means to
+f = int(input('From: '))
+t = int(input('To: '))
+
+# stores the random num
+random_num = random.randint(f,t)
+
+# the allowed attempts 
+attempts = int(input("Select number of attempts you want (MAX 10): "))
+
+# the user is guessing
+usr_guess = int(input('Guess a number between {} and {}: '.format(f,t)))
+
 # This loop checks if the gussed num == the random
 # the loop will break if the user failed to guess the number after 3 attempts
 while attempts > 1:
@@ -17,16 +23,13 @@ while attempts > 1:
     if usr_guess == random_num:
         print("Good job! You Guessed it!")
         break
-    elif diff < 10:
+    else:
         attempts -= 1
         print('So close! Try {} more time(s)'.format(attempts))
-        usr_guess = int(input('Guess a number between 1 and 100: '))
+        usr_guess = int(input('Guess a number between 1 and 20: '))
     
 else:
-    print("GAME OVER!")
-
-
-		
+    print("GAME OVER! Try again")
 
 	
 
