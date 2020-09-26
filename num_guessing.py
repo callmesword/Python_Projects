e39d60d10135c3f745b6df534796c16ca4a0b423
0XLaya <61026156+0XLAYA@users.noreply.github.com>
2020-09-26 15:02:53 +0100

    Update num_guessing.py

--- num_guessing.py
+++ num_guessing.py
@@ -1,29 +1,31 @@
 import random
-# a function to compute the difference between the guessed num and the random num
-def diff(usr_inp,random_num):
-	return random_num - usr_inp
+
+#the range the user wants, F means from and T means To
+f = int(input("From: "))
+t = int(input("To: "))
 
 # stores the random num
-random_num = random.randint(1,50)
+random_num = random.randint(f,t)
+
+# the allowed attempts the user wants
+att = int(input("How many times you wanna attempt? : "))
 
 # the user is guessing
-usr_guess = int(input('Guess a number between 1 and 50: '))
+usr_guess = int(input('Guess a number between {} and {}: '.format(f,t)))
 
-# the allowed attempts 
-attempts = 3
 
 # This loop checks if the gussed num == the random
 # the loop will break if the user failed to guess the number after 3 attempts
 
-while attempts > 1:
+while att > 1:
 	
 	if usr_guess == random_num:
 		print("Good job! You Guessed it!")
 		break
 	else:
-		attempts -= 1
-		print('So close! Try {} more time(s)'.format(attempts))
-		usr_guess = int(input('Guess a number between 1 and 100: '))
+		att -= 1
+		print('So close! Try {} more time(s)'.format(att))
+		usr_guess = int(input('Guess a number between {} and {}: '.format(f,t)))
 	
 else:
 	print("GAME OVER!")
