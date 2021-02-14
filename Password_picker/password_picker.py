733268202ec31d67d4f8cb253e429ed3a2cea3f9
0XLaya <61026156+0XLAYA@users.noreply.github.com>
2021-02-15 00:27:31 +0100

    Update password_picker.py

--- Password_picker/password_picker.py
+++ Password_picker/password_picker.py
@@ -20,22 +20,26 @@ def password():
 	num = random.choice(string.digits)
 	#random punctuation
 	punc = random.choice(string.punctuation)
-	return rand_adj+rand_noun+num+num+punc+punc
+	return rand_adj+rand_noun+num*4+punc*4
 
 #output message
-print("welcome to 'Pick me a strong password' program")
-print("###############################################")
-print("Type 'pass' to generate a password and 'exit' to exit the program")
-print("###############################################")
+print("\n<<<<< welcome to 'Pick me a strong password' program >>>>>\n")
+print("+-------------------------------------------------------------------+")
+print("| Type 'pass' to generate a password and 'exit' to exit the program |")
+print("+-------------------------------------------------------------------+")
+
 
 user = str(input("==> "))
+    
 while user != 'exit':
 	if user == 'pass':
 		print("your password is:",string.capwords(password()))
 		user = str(input("==> "))
 else:
 	if user == 'exit':
-		print("Thanks for using the program")
+		print("+------------------------------+")
+		print("| Thanks for using the program |")
+		print("+------------------------------+")
 
 adjs_file.close()
 nouns_file.close()
