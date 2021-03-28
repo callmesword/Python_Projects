3cdcf50ae75e3fbc66eb8b5213c1b3f29cedc9bd
Houssam Lahyani <61026156+HOUSSAM-LAYANI@users.noreply.github.com>
2021-03-28 13:34:20 +0100

    Update dice.py

--- dice.py
+++ dice.py
@@ -35,15 +35,15 @@ def start():
 			p1_choice = int(input("Enter a num between 1 & 6:  "))
 		if p1_choice in range(1,7):
 			print("ROOLING.....")
-			time.sleep(1)
+			time.sleep(0.5)
 			if p1_choice == dice:
 				print("+----------------------+")
 				print("| Good job! keep going |")
 				print("+----------------------+")
 				p1.add_score(p1.score)
-				print("+-----------------------+")
-				print("| Your score so far is: |",p1.score)
-				print("+-----------------------+")
+				print("+-------------------------+")
+				print("| Your score so far is: {}|".format(p1.score))
+				print("+-------------------------+")
 			else:
 				tries -= 1
 				print("+------------------+")
@@ -74,15 +74,15 @@ def start():
 				p2_choice = int(input("Enter a num between 1 & 6:  "))
 			if p2_choice in range(1,7):
 				print("ROOLING.....")
-				time.sleep(1)
+				time.sleep(0.5)
 				if p2_choice == dice:
 					print("+----------------------+")
 					print("| Good job! keep going |")
 					print("+----------------------+")
 					p2.add_score(p2.score)
-					print("+-----------------------+")
-					print("| Your score so far is: |",p2.score)
-					print("+-----------------------+")
+					print("+-------------------------+")
+					print("| Your score so far is: {} |".format(p2.score))
+					print("+-------------------------+")
 				else:
 					tries -= 1
 					print("+------------------+")
@@ -97,23 +97,35 @@ def start():
 			if p1.score > p2.score:
 				winner = p1.name
 				print("LOADING RESULTS......")
-				time.sleep(0.4)
+				time.sleep(1)
+				print(p1.name+"'s score is:",p1.score)
+				time.sleep(1)
+				print(p2.name+"'s score is:",p2.score)
 				print("The winner is:\n")
-				time.sleep(0.5)
+				time.sleep(1)
 				print("+-----------+")
-				print('|',p1.name,'|')
+				print('|',winner,'|')
 				print("+-----------+")
 			elif p2.score > p1.score:
 				winner = p2.name
 				print("LOADING RESULTS......")
-				time.sleep(0.4)
+				print(p1.name+"'s score is:",p1.score)
+				time.sleep(1)
+				print(p2.name+"'s score is:",p2.score)
+				time.sleep(1)
 				print("The winner is:\n")
-				time.sleep(0.5)
+				time.sleep(1)
 				print("+-----------+")
-				print('|',p2.name,'|')
+				print('|',winner,'|')
 				print("+-----------+")
 			else:
+				print("<><><><><><><><><><><><>")
 				print("LOADING RESULTS......")
+				time.sleep(1)
+				print(p1.name+"'s score is:",p1.score)
+				time.sleep(1)
+				print(p2.name+"'s score is:",p2.score)
+				time.sleep(1)
 				print("+------+")
 				print("| TIE! |")
 				print("+------+")
