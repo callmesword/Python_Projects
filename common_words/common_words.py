026700f0cc1925ed1ed0d594a5f8d82f25376f8d
Houssam Lahyani <61026156+HOUSSAM-LAYANI@users.noreply.github.com>
2021-05-15 20:06:09 -0500

    Add files via upload

---
+++ common_words/common_words.py
@@ -0,0 +1,23 @@
+import string
+punc = string.punctuation
+
+def common_word(dictt):
+	common = []
+	for element in dictt:
+		if dictt[element] > 1:
+			common.append(element)
+	return common
+
+
+with open("string.txt") as txt:
+	txt = txt.readlines()
+	words_dict = {}
+	for sentence in txt:
+		for word in sentence.split(" "):
+			word = word.rstrip("\n" + punc)
+			if word not in words_dict:
+				words_dict[word] = 1
+			else:
+				words_dict[word] += 1
+
+print("These words appeared more than once:\n",common_word(words_dict))
