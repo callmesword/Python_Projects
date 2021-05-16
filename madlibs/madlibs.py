345834081adc7e27d734785e289a3dae4742d357
Houssam Lahyani <61026156+HOUSSAM-LAYANI@users.noreply.github.com>
2021-05-15 20:10:38 -0500

    madlibs

---
+++ madlibs/madlibs.py
@@ -0,0 +1,21 @@
+# Make sure the madlibs.txt file is in the same folder as madlibs.py before you try to run this code
+
+import random
+ 
+# Open the Mad Libs text file
+f = open('madlibs.txt','r')
+ 
+# Read the whole file and store each line in a list
+madlibText = f.readlines()
+ 
+# Choose a random line from the list
+madlib = random.choice(madlibText)
+ 
+# Ask the user to input a noun
+noun = input("Enter a noun: ")
+ 
+# Replace the blank with the user's input
+madlib = madlib.replace("blank", noun)
+ 
+# Print out the Mad Lib including the user's response
+print(madlib)
